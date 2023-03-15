06f8c45803d4345df9df75b859f90dbac0cd3a04
Daniel Nikolov <danielsnikolov@gmail.com>
2023-03-15 18:23:56 +0200

    Added new Interfaces and record versions of the models to allow for record models to be used in the system.

--- EFCoreRepositoriesLib/SourceCode/PrimaryKeyUser.cs
+++ EFCoreRepositoriesLib/SourceCode/PrimaryKeyUser.cs
@@ -7,22 +7,48 @@ using System.Threading.Tasks;
 
 namespace EFCoreRepositoriesLib;
 
-internal interface IPrimaryKeyUser
+public interface IPrivatePrimaryKeyUser
 {
 }
 
-public class PrivatePrimaryKeyUser : IPrimaryKeyUser
+public interface IReadOnlyPrimaryKeyUser : IPrivatePrimaryKeyUser
+{
+    public int ID { get; }
+}
+
+public interface IPublicPrimaryKeyUser : IReadOnlyPrimaryKeyUser
+{
+    public new int ID { get; set; }
+}
+
+public class PrivatePrimaryKeyUser : IPrivatePrimaryKeyUser
 {
     [AdaptMember(nameof(PublicPrimaryKeyUser.ID))]
     private protected int _id { get; set; }
 }
 
-public class ReadOnlyPrimaryKeyUser : PrivatePrimaryKeyUser
+public class ReadOnlyPrimaryKeyUser : PrivatePrimaryKeyUser, IReadOnlyPrimaryKeyUser
 {
     public int ID { get => _id; private set => _id = value; }
 }
 
-public class PublicPrimaryKeyUser : ReadOnlyPrimaryKeyUser
+public class PublicPrimaryKeyUser : ReadOnlyPrimaryKeyUser, IPublicPrimaryKeyUser
+{
+    public new int ID { get => _id; set => _id = value; }
+}
+
+public record class RecordPrivatePrimaryKeyUser : IPrivatePrimaryKeyUser
+{
+    [AdaptMember(nameof(PublicPrimaryKeyUser.ID))]
+    private protected int _id { get; set; }
+}
+
+public record class RecordReadOnlyPrimaryKeyUser : RecordPrivatePrimaryKeyUser, IReadOnlyPrimaryKeyUser
+{
+    public int ID { get => _id; private set => _id = value; }
+}
+
+public record class RecordPublicPrimaryKeyUser : RecordReadOnlyPrimaryKeyUser, IPublicPrimaryKeyUser
 {
     public new int ID { get => _id; set => _id = value; }
 }
